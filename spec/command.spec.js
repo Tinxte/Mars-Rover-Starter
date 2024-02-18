ffc11e5a5fd3822a058294da5b5d91c83cc42555
unknown <150742495+Tinxte@users.noreply.github.com>
2024-02-18 14:41:20 -0600

    Message Tests

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -10,3 +10,22 @@ describe("Command class", function() {
   });
 
 });
+
+// //Test 2: Create a second Command test using “constructor sets command type” as the description. 
+
+describe("Command class", function() {
+ let testClass = new Command("input");
+  it("constructor sets command type", function() {
+    expect(testClass.commandType).toBe("input");
+  });
+
+});
+
+//Test 3: This test checks that the constructor correctly sets the value property in the new object.
+
+describe("Command class", function() {
+let testClass = new Command("input1", "input2");
+  it("constructor sets a value passed in as the 2nd argument", function () {
+    expect(testClass.value).toBe("input2");
+  });
+});
