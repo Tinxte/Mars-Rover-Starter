a207761d178c55a713eefbe07e896ac087ee0be3
unknown <150742495+Tinxte@users.noreply.github.com>
2024-02-18 19:29:32 -0600

    Test 5 written

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -4,6 +4,21 @@ const Command = require('../command.js');
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe("Message class", function() {
 
+// Test 4 
+
+describe("Message class", function() {
+        it('throws error if a name is NOT passed into the constructor as the first parameter', function () {
+            expect( function () {new Message()}).toThrow("Error! Name required.");
+        });
+});
+
+
+// Test 5
+
+describe("Message class", function() {
+    let testMessage = new Message("Test Name");
+    it('constructor sets name', function (){
+        expect(testMessage.name).toBe("Test Name");
+    });
 });
