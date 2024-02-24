d651c86c9ffb7edc74777bbf78a99057d5fb3f00
unknown <150742495+Tinxte@users.noreply.github.com>
2024-02-24 12:31:55 -0600

    Test 8 completed

--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -9,8 +9,33 @@ const Command = require('../command.js');
 // Test 7: constructor sets position and default values for mode and generatorWatts
 
 describe("Rover class", function() {
+  let testRover = new Rover(98382)
   it('constructor sets position and default values for mode and generatorWatts', function () {
-      expect().ToBe()
+      expect(testRover.position).toBe(98382);
+      expect(testRover.mode).toBe("NORMAL");
+      expect(testRover.generatorWatts).toBe(110);
   });
+});
 
+// Test 8: “response returned by receiveMessage contains the name of the message”
+
+describe("Rover class", function() {
+  let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+  let message = new Message('Test message with two commands', commands);
+  let testRover = new Rover(98382)
+    it('response returned by receiveMessage contains the name of the message', function () {
+    expect(testRover.receiveMessage(message)).toContain('Test message with two commands');
+  });
+});
+
+//TODO 
+// Test 9: “response returned by receiveMessage includes two results if two commands are sent in the message”
+
+describe("Rover class", function() {
+  let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+  let message = new Message('Test message with two commands', commands);
+  let testRover = new Rover(98382)
+    it('response returned by receiveMessage includes two results if two commands are sent in the message', function () {
+    expect(testRover.receiveMessage(message)).toBe('Test message with two commands');
+  });
 });
